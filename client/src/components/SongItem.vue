8e9ac1bef15cd98c3437a686ff3a8e48eb7b4102
robinhlgrsn <robinholgersson91@gmail.com>
2021-08-28 18:21:24 +0200

    edit, delete uploaded songs
    Created vuex store with actions and mutations for editing and deleting uploaded songs.

---
+++ client/src/components/SongItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="flex border justify-between px-5 mx-4 my-2 py-1" v-show="!isEditing">
+      <div class="flex">
+      <h1 class="font-bold mx-2">{{artist}}</h1>
+        -
+      <p class="mx-2">{{title}}</p>
+      </div>
+      <div>
+        <i  @click.prevent="isEditing = !isEditing" class="mx-4 fas fa-pencil-alt"></i>
+        <i @click.prevent="removeSong(id)" class="text-red-500 fas fa-times"></i>
+      </div>
+    </div>
+      <form @submit.prevent="updateSong(id)" class="flex border justify-between px-5 mx-4 my-2 py-1" v-show="isEditing">
+        <div class="flex flex-col">
+          <input v-model="updatedArtist" class="border py-1 px-4 mx-2 rounded font-bold" placeholder="Artist" type="text">
+          <input v-model="updatedTitle" class="border py-1 px-4 mx-2 rounded font-bold" placeholder="Song Title" type="text">
+        </div>
+        <div class="flex flex-col">
+          <button class="border-2 py-1 px-4 rounded-full font-bold" >Save</button>
+          <button class="border-2 py-1 px-4 rounded-full font-bold" @click.prevent="isEditing = !isEditing">Back</button>
+        </div>
+        
+      </form>
+  <div>
+
+  </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'SongItem',
+  data() {
+    return {
+      isEditing: false,
+      updatedArtist: null,
+      updatedTitle: null,
+    }
+  },
+  props: {
+    artist: String,
+    title: String,
+    id: String,
+  },
+  methods: {
+    updateSong(id) {
+      console.log(this.updatedArtist)
+      console.log(this.updatedTitle)
+      this.$store.dispatch('updateSong', {
+        id,
+        artist: this.updatedArtist,
+        title: this.updatedTitle
+      })
+      this.isEditing = false; 
+    },
+    removeSong(id) {
+      console.log('heyaa')
+      this.$store.dispatch('removeSong', { id });
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/Manage.vue
+++ client/src/views/Manage.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="container mx-auto my-32">
     <section class="flex flex-col items-center border rounded-lg shadow-lg">
-      <header class="text-5xl my-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+      <header class="py-1 text-5xl my-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
         Upload
       </header>
       <AppForm />
+    </section>
+     <section class="flex flex-col items-center border my-10 rounded-lg shadow-lg" v-show="uploadedSongs.length > 0">
+      <header class="py-1 text-5xl my-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+        Songs
+      </header>
+      <div class="flex flex-col w-2/3" v-for="song in uploadedSongs" :key="song.id">
+        <SongItem :artist="song.artist" :id="song.id" :title="song.songTitle" />
+      </div>
     </section>
   </div>
 
 </template>
 
 <script>
-import AppForm from '@/components/Form.vue'
+import AppForm from '@/components/Form.vue';
+import SongItem from '@/components/SongItem.vue';
+import { mapState } from 'vuex';
+
 export default {
   name: 'manage',
   components: {
     AppForm,
+    SongItem,
   },
+  computed: {
+    ...mapState(['uploadedSongs'])
+  }
 }
 </script>
 
